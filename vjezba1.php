f858499279501cf77af1a28af54d5d8f9e1ac69b
nmiokovic <nmiokovic@tvz.hr>
2026-03-25 00:19:31 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,9 @@
+<?php
+$naslov = "Moja prva PHP stranica";
+$autor = "Nina Mioković";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradila je $autor.</p>";
+echo '<p><a href="https://www.google.com" target="_blank">Posjeti Google</a></p>';
+?>
+<!-- vjezba1.php -->
